40e560370dbc99d463e4dd5631615b9e0339761f
Wellington Santos de Carvalho <wellslls@hotmail.com>
2019-10-23 09:44:56 -0300

    Primeira atualização

---
+++ AloGitHub/AloGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro teste com o GitHub");
+            Console.ReadKey();
+        }
+    }
+}
