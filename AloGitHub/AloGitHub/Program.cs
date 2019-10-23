6f63a04819e189223262dc4507b95e40a11694db
Wellington Santos de Carvalho <wellslls@hotmail.com>
2019-10-23 11:02:04 -0300

    Nova função adicionada chamada EscreveNaTela

--- AloGitHub/AloGitHub/Program.cs
+++ AloGitHub/AloGitHub/Program.cs
@@ -4,9 +4,13 @@ namespace AloGitHub
 {
     class Program
     {
-        static void Main(string[] args)
+        static void EscreveNaTela()
         {
             Console.WriteLine("Primeiro teste com o GitHub");
+        }
+        static void Main(string[] args)
+        {
+            EscreveNaTela();
             Console.ReadKey();
         }
     }
